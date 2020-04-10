75be8ff6720eae17cb968a1b2ab68782ca976798
Satyam Rawalji <srawalji1@gmail.com>
2020-04-10 02:14:34 -0700

    First commit for site

---
+++ src/build/precache-manifest.24798bf13063c70ec38b57685c6c8053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5d017c4a6216e9cc21f164f4d8ee08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd21bb47a3e2aa51eb8b",
+    "url": "/static/css/main.84b4ca96.chunk.css"
+  },
+  {
+    "revision": "2ff03700911daf271c5f",
+    "url": "/static/js/2.461ad15f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.461ad15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd21bb47a3e2aa51eb8b",
+    "url": "/static/js/main.dbdb9c99.chunk.js"
+  },
+  {
+    "revision": "209d7516a220ea251298",
+    "url": "/static/js/runtime-main.cc5c5978.js"
+  }
+]);
